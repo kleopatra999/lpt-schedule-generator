15ef1022f5aa13add9f6326a227657e2deb00f66
quietfanatic <lewis@wall.org>
2013-03-21 13:37:42 -0700

    Allow repeating the calculation for benchmarking

--- main.cpp
+++ main.cpp
@@ -31,49 +31,67 @@ std::vector<std::string> split (std::string s, char delim) {
 
 void usage (int argc, char** argv, std::string mess = "");
 
-int run (uint n_machines, const std::vector<uint>& tasks, bool optimal, bool verbose) {
+int run (uint n_machines, const std::vector<std::vector<uint> >& taskses, bool optimal, bool verbose, uint reps, bool just_time) {
     using namespace std;
+    clock_t pre = clock();
     if (optimal) {
         if (verbose) {
             cerr << "Cannot use both -v (verbose) and -o (optimal)." << endl;
             return 127;
         }
-        cout << optimal_time(n_machines, tasks) << endl;
-        return 0;
-    }
-    const Schedule& s = lpt(n_machines, tasks);
-    if (verbose) {
-        cout << "===";
-        for (uint i = 0; i < tasks.size(); i++) {
-            cout << tasks[i];
-            if (i != tasks.size() - 1) cout << " ";
-        }
-        cout << endl;
-        cout << " # 0";
-        for (uint i = 5; i < s.total_time; i += 5) {
-            cout << std::setw(5) << i;
-        }
-        cout << endl << "   =";
-        for (uint i = 0; i < s.total_time; i++) cout << "=";
-        cout << endl;
-        for (uint m = 0; m < n_machines; m++) {
-            cout << std::setw(2) << m << " |";
-            for (uint i = 0; i < s.allocations[m].size(); i++) {
-                if (s.allocations[m][i] > 0) {
-                    cout << std::setw(s.allocations[m][i]) << "|";
-                }
+        for (uint i = 0; i < taskses.size(); i++) {
+            for (uint r = reps - 1; r; r--) {
+                optimal_time(n_machines, taskses[i]);
             }
-            cout << endl << "   =";
-            for (uint i = 0; i < s.total_time; i++) cout << "=";
-            cout << endl;
+            if (!just_time)
+                cout << optimal_time(n_machines, taskses[i]) << endl;
         }
-        cout << "Total time: " << s.total_time << endl;
-        return 0;
     }
     else {
-        cout << s.total_time << endl;
-        return 0;
+        for (uint i = 0; i < taskses.size(); i++) {
+            for (uint r = reps - 1; r; r--) {
+                lpt(n_machines, taskses[i]);
+            }
+            const Schedule& s = lpt(n_machines, taskses[i]);
+            if (verbose) {
+                cout << "===";
+                for (uint j = 0; j < taskses[j].size(); j++) {
+                    cout << taskses[i][j];
+                    if (j != taskses[i].size() - 1) cout << " ";
+                }
+                cout << endl;
+                cout << " # 0";
+                for (uint j = 5; j < s.total_time; j += 5) {
+                    cout << std::setw(5) << j;
+                }
+                cout << endl << "   =";
+                for (uint j = 0; j < s.total_time; j++) cout << "=";
+                cout << endl;
+                for (uint m = 0; m < n_machines; m++) {
+                    cout << std::setw(2) << m << " |";
+                    for (uint j = 0; j < s.allocations[m].size(); j++) {
+                        if (s.allocations[m][j] > 0) {
+                            cout << std::setw(s.allocations[m][j]) << "|";
+                        }
+                    }
+                    cout << endl << "   =";
+                    for (uint j = 0; j < s.total_time; j++) cout << "=";
+                    cout << endl;
+                }
+                cout << "Total time: " << s.total_time << endl;
+            }
+            else if (!just_time) {
+                cout << s.total_time << endl;
+            }
+        }
     }
+    clock_t post = clock();
+    double spent = (post - pre) * 1.0 / CLOCKS_PER_SEC;
+    if (just_time)
+        cout << spent << endl;
+    else
+        cout << "Approximate calculation time: " << spent << "s" << endl;
+    return 0;
 }
 
 int main (int argc, char** argv) {
@@ -89,13 +107,26 @@ int main (int argc, char** argv) {
     bool verbose = false;
     bool optimal = false;
     bool from_file = false;
+    bool want_reps = false;
+    bool just_time = false;
+    uint reps = 1;
     for (int i = 1; i < argc; i++) {
         string arg = (const char*)argv[i];
-        if (arg == "-v") {
+        if (want_reps) {
+            stringstream(arg) >> reps;
+            want_reps = false;
+        }
+        else if (arg == "-v") {
             verbose = true;
         }
         else if (arg == "-o") {
             optimal = true;
+        }
+        else if (arg == "-n") {
+            want_reps = true;
+        }
+        else if (arg == "-t") {
+            just_time = true;
         }
         else if (arg == "-f") {
             if (!tasks.empty()) {
@@ -125,7 +156,6 @@ int main (int argc, char** argv) {
     }
      // Read tasks from files if necessary.
     if (from_file) {
-        vector<vector<uint> > taskses;
          // C++ IS VERY BLETCHFUL
         for (vector<string>::iterator fsi = filenames.begin(); fsi != filenames.end(); fsi++) {
             ifstream f (fsi->c_str());
@@ -133,7 +163,7 @@ int main (int argc, char** argv) {
             string file = string(istreambuf_iterator<char>(f), istreambuf_iterator<char>());
             f.close();
             const vector<string>& lines = split(file, '\n');
-            taskses.resize(lines.size());
+            vector<vector<uint> > taskses (lines.size());
             for (uint li = 0; li < lines.size(); li++) {
                 const vector<string>& parts = split(lines[li], '\t');
                 taskses[li].resize(parts.size());
@@ -141,17 +171,17 @@ int main (int argc, char** argv) {
                     stringstream(parts[pi]) >> taskses[li][pi];
                 }
             }
-        }
-        if (verbose) cout << "===NUMBER OF INSTANCES: " << taskses.size() << endl;
-        for (vector<vector<uint> >::iterator tli = taskses.begin(); tli != taskses.end(); tli++) {
-            int ret = run(n_machines, *tli, optimal, verbose);
-            if (ret != 0)
+            if (verbose) cout << "===NUMBER OF INSTANCES: " << taskses.size() << endl;
+            int ret = run(n_machines, taskses, optimal, verbose, reps, just_time);
+            if (ret != 0) {
+                cerr << "Calculation aborted due to error" << endl;
                 return ret;
+            }
         }
         return 0;
     }
     else {
-        return run(n_machines, tasks, optimal, verbose);
+        return run(n_machines, std::vector<std::vector<uint> >(1, tasks), optimal, verbose, reps, just_time);
     }
 
 }
@@ -166,5 +196,7 @@ void usage (int argc, char** argv, std::string mess) {
     cerr << "    -f: Read lists of tasks from the given file(s) with one space-separated list of numbers per line." << endl;
     cerr << "    -v: Print out a chart of the selected schedule as well as the total schedule length." << endl;
     cerr << "    -o: Print the optimal schedule length (incompatible with -v).  Takes exponential CPU time." << endl;
+    cerr << "    -n [reps]: Repeat the calculations this many times, for benchmarking." << endl;
+    cerr << "    -t: Print nothing but the time the calculation took, in seconds." << endl;
 }
 
